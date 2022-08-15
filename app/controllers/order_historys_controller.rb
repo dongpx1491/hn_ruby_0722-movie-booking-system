51472b74720f32ea399bb5d1662687575fd58ed7
Nguyen Nhat <quocnhat1503@gmail.com>
2022-08-15 14:13:29 +0700

    Add gem devise to login,logout,signup

--- app/controllers/order_historys_controller.rb
+++ app/controllers/order_historys_controller.rb
@@ -1,6 +1,6 @@
 class OrderHistorysController < ApplicationController
-  before_action :logged_in_user
   authorize_resource class: false
+  before_action :authenticate_user!
 
   def index
     @payments = current_user.payments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_103851) do
+ActiveRecord::Schema.define(version: 2022_09_12_020320) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -154,13 +154,16 @@ ActiveRecord::Schema.define(version: 2022_08_28_103851) do
     t.integer "role", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "remember_digest"
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
