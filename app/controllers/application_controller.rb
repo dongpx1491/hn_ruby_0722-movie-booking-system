d41166c3791b66ad27a2396c574267a0ad27158c
dong_px <pxd1491.forwork@gmail.com>
2022-10-12 11:00:17 +0700

    api movie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include PaymentsHelper
   include FavoritesHelper
   include Admin::PaymentsHelper
